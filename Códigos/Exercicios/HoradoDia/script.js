f36780a9250be52b3be4116d6f3b29aa7bfefaed
Guilherme Duarte <kinggui2011@hotmail.com>
2022-07-11 16:31:09 -0300

    Update11.07

--- Códigos/Exercicios/HoradoDia/script.js
+++ Códigos/Exercicios/HoradoDia/script.js
@@ -4,5 +4,13 @@ function carregar(){
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
-
+    if (hora >= 0 && hora < 12){
+        img.src = 'imgs/manhã.jpg'
+    } else if (hora >= 12 && hora < 18){
+        img.src = 'imgs/tarde.jpg'                
+        document.body.style.background = '#F7DC6F'
+    } else {
+        img.src = 'imgs/noite.jpg'
+        document.body.style.background = '#34495E '
+    }
 }
