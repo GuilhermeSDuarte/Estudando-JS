850bcff3f0dbee716f7267decc7314b268d30d24
Guilherme Duarte <kinggui2011@hotmail.com>
2022-07-18 15:31:25 -0300

    UpdateTem.Exer

--- Códigos/Exercicios/IdadePessoa/script.js
+++ Códigos/Exercicios/IdadePessoa/script.js
@@ -8,6 +8,21 @@ function verificar() {
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)   
-        
+        var genero = ''
+        if(fsex[0].checked){
+            genero = 'Masculino'
+        } else {
+            genero = 'Feminino'
+        }
+
+        if(idade < 14){
+            res.innerHTML = `Idade Calculada ${idade} / Você é uma criança do genero ${genero}.`
+        } else if (idade >= 14 && idade < 26){
+            res.innerHTML = `Idade Calculada ${idade} / Você é um adolescente do genero ${genero}.`
+        } else if (idade >= 26 && idade < 61){
+            res.innerHTML = `Idade Calculada ${idade} / Você é um adulto do genero ${genero}.`
+        } else {
+            res.innerHTML = `Idade Calculada ${idade} / Você é um idoso do genero ${genero}.`
+        }
     }
 }
